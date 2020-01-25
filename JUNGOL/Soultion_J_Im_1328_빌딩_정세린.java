a0c449efc0935708da820047bfece4c27c559c87
Serin <tpfls410@gmail.com>
2020-01-25 14:24:45 +0000

    Upload New File

---
+++ JUNGOL/Soultion_J_Im_1328_빌딩_정세린.java
@@ -0,0 +1,30 @@
+package jungol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Soultion_J_Im_1328_빌딩_정세린 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] H = new int[N];
+		int look;
+		for (int i = 0; i < N; i++) {
+			H[i] = Integer.parseInt(br.readLine());
+		}
+
+		for (int i = 0; i < N; i++) {
+			look = 0;
+			for (int j = i + 1; j < N; j++) {
+				if (H[i] < H[j]) {
+					look = j + 1;
+					break;
+				}
+			}
+			System.out.println(look);
+		}
+	}
+}
