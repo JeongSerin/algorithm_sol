5c0a9829583027501f4e2bb41a0b90b8141dbaf3
Serin <tpfls410@gmail.com>
2020-01-26 07:45:53 +0000

    Upload New File

---
+++ JUNGOL/Solution_J_Be_1161_하노이1_정세린.java
@@ -0,0 +1,26 @@
+package jungol;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_J_Be_1161_하노이1_정세린 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // 원판 개수
+
+		hanoi(N, 0, 1, 2);
+	}
+
+	private static void hanoi(int n, int i, int t, int j) {
+		if (n == 1) {
+			System.out.printf("%d : %d -> %d\n", n, i + 1, j + 1);
+		} else {
+			hanoi(n - 1, i, j, t);
+			System.out.printf("%d : %d -> %d\n", n, i + 1, j + 1);
+			hanoi(n - 1, t, i, j);
+		}
+
+	}
+}
