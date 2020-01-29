0a651ced724a1773e3bfb4c0977929560928f560
Serin <tpfls410@gmail.com>
2020-01-29 08:25:54 +0000

    Upload New File

---
+++ ssafy_exercise/0123/ArraysTest.java
@@ -0,0 +1,18 @@
+package com.ssafy.array;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArraysTest {
+	public static void main(String[] args) {
+		int[] arr = {5, 4, 8, 3, 6};
+		Arrays.sort(arr);
+		
+		Integer[] array1 = {5, 4, 8, 3, 6};
+		Arrays.sort(array1, Collections.reverseOrder());
+		
+		for (Integer a : array1) {
+			System.out.print(a + " ");
+		}
+	}
+}
