248719ba2515b27f66ec89343fbaca991b6cd25a
Serin <tpfls410@gmail.com>
2020-01-29 09:44:56 +0000

    Update Solution_J_Be_1303_숫자사각형1_정세린.java

--- JUNGOL/Main_J_Be_1303_숫자사각형1_정세린.java
+++ JUNGOL/Main_J_Be_1303_숫자사각형1_정세린.java
@@ -1,11 +1,9 @@
-package jungol;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_J_Be_1303_숫자사각형1_정세린 {
+public class Main_J_Be_1303_숫자사각형1_정세린 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
