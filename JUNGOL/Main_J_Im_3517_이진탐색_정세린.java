679a470ce52440f1612cfeba057fed6671f805ff
Serin <tpfls410@gmail.com>
2020-01-29 09:48:03 +0000

    Update Solution_J_Im_3517_이진탐색_정세린.java

--- JUNGOL/Main_J_Im_3517_이진탐색_정세린.java
+++ JUNGOL/Main_J_Im_3517_이진탐색_정세린.java
@@ -1,12 +1,10 @@
-package jungol;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution_J_Im_3517_이진탐색_정세린 {
+public class Main_J_Im_3517_이진탐색_정세린 {
 	static int[] ai, bi;
 
 	public static void main(String[] args) throws IOException {
