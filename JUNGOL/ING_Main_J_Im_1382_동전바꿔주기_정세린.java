d627e6b017dc69c5b107788a66a3dcf96b478320
Serin <tpfls410@gmail.com>
2020-02-03 04:28:33 +0000

    Update ING_Solution_J_Im_1382_동전바꿔주기_정세린.java

--- JUNGOL/ING_Main_J_Im_1382_동전바꿔주기_정세린.java
+++ JUNGOL/ING_Main_J_Im_1382_동전바꿔주기_정세린.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_J_Im_1382_동전바꿔주기_정세린 {
+public class Main_J_Im_1382_동전바꿔주기_정세린 {
 	static int T, k;
 	static int[] p, n;
 	static int cnt = 0, sum = 0;
