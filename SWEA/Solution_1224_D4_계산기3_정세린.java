c08117976183edc1009fe8b9607298d3f667e4e1
Serin <tpfls410@gmail.com>
2020-02-05 09:21:47 +0000

    Replace Solution_1224_D4_계산기3_정세린.java

--- SWEA/Solution_1224_D4_계산기3_정세린.java
+++ SWEA/Solution_1224_D4_계산기3_정세린.java
@@ -1,123 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.StringTokenizer;
 
-public class Solution_1224_D4_계산기3_정세린 {
-	static StringBuilder res = new StringBuilder();
+public class Solution_D3_1234_비밀번호_정세린 {
+	static String str;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-//		System.setIn(new FileInputStream("input.txt"));
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int len;
-		String str;
-		Stack<Character> st = new Stack<Character>();
+		int t;
+		String str1, str2;
 		StringBuilder sb = new StringBuilder();
-		char tmp, top;
+		StringTokenizer st;
 
 		for (int tc = 1; tc <= 10; tc++) {
-			sb = new StringBuilder();
-			len = Integer.parseInt(br.readLine());
-			str = br.readLine();
-
-			for (int i = 0; i < len; i++) {
-				tmp = str.charAt(i);
-
-				if (Character.isDigit(tmp)) {
-					sb.append(tmp);
-				} else if (tmp == ')') {
-					// 여는괄호 만날때 까지 팝
-					while (!st.isEmpty() && (top = st.pop()) != '(') {
-						sb.append(top);
+			st = new StringTokenizer(br.readLine(), " ");
+			t = Integer.parseInt(st.nextToken());
+			str = st.nextToken();
+			L: while (true) {
+				for (int i = 1; i < str.length(); i++) {
+					if (str.charAt(i - 1) == str.charAt(i)) {
+						str1 = str.substring(0, i - 1);
+						str2 = str.substring(i + 1, str.length());
+						str = str1.concat(str2);
+						continue L;
 					}
-				} else {
-
-					while (!st.isEmpty() && inComingPriority(tmp) <= inStackPriority(st.peek())) {
-						sb.append(st.pop());
-
-					}
-					st.push(tmp);
 				}
+				break;
+
 			}
-
-			while (!st.isEmpty())
-				sb.append(st.pop());
-
-			res.append('#').append(tc).append(' ').append(calculate(sb)).append('\n');
-//			System.out.println(sb.toString());
+			sb.append('#').append(tc).append(' ').append(str.toString()).append('\n');
 		}
-		System.out.println(res.toString());
+		System.out.println(sb.toString());
 	}
-
-	private static int inStackPriority(char ch) {
-		int pri = 0;
-		switch (ch) {
-		case '(':
-			pri = 0;
-			break;
-		case '+':
-		case '-':
-			pri = 1;
-			break;
-		case '*':
-		case '/':
-			pri = 2;
-			break;
-		}
-		return pri;
-	}
-
-	private static int inComingPriority(char ch) {
-		int pri = 0;
-		switch (ch) {
-		case '(':
-			pri = 3;
-			break;
-		case '+':
-		case '-':
-			pri = 1;
-			break;
-		case '*':
-		case '/':
-			pri = 2;
-			break;
-		}
-		return pri;
-	}
-
-	private static int calculate(StringBuilder str) {
-		char tmp;
-		int a, b;
-		Stack<Integer> calst = new Stack<Integer>();
-
-		for (int i = 0; i < str.length(); i++) {
-			tmp = str.charAt(i);
-			// 연산자면 팝하면서 계산
-			if (tmp == '+' || tmp == '*' || tmp == '/' || tmp == '-') {
-				b = calst.pop();
-				a = calst.pop();
-
-				switch (tmp) {
-				case '+':
-					calst.push(a + b);
-					break;
-				case '*':
-					calst.push(a * b);
-					break;
-				case '/':
-					calst.push(a / b);
-					break;
-				case '-':
-					calst.push(a - b);
-					break;
-				}
-			}
-			// 연산자가 아니라 숫자
-			else {
-				calst.push(tmp - '0');
-			}
-		}
-		return calst.peek();
-	}
-
 }
