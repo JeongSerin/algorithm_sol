1cd76cceb30d779c6e6b7566eade5d59cb996f19
Serin <tpfls410@gmail.com>
2020-02-07 09:50:37 +0000

    Update Main_J_Im_2752_개미_정세린2.java

--- ING_Main_J_Im_2752_개미_정세린.java
+++ ING_Main_J_Im_2752_개미_정세린.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_J_Im_2752_개미_정세린2 {
+public class Main_J_Im_2752_개미_정세린 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
