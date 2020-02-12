f83a140bffb1df5b246047b2cf4f4820e71b876b
Serin <tpfls410@gmail.com>
2020-02-12 00:30:19 +0000

    Update Main_B_G5_17471_게리맨더링_정세린.java

--- BAEKJOON/Main_B_G5_17471_게리맨더링_정세린.java
+++ BAEKJOON/Main_B_G5_17471_게리맨더링_정세린.java
@@ -44,7 +44,6 @@ public class Main_B_G5_17471_게리맨더링_정세린 {
 		}
 
 		L: for (int i = 1; i < (1 << (N - 1)); i++) {
-			
 			// 새로 조합 만들면 visited, subsum초기화
 			subsum0 = 0;
 			Arrays.fill(visited, false);
