e95ea6de04fb88ed7d464a6fd0ba072326431fb7
Serin <tpfls410@gmail.com>
2020-02-14 00:30:57 +0000

    Upload New File

---
+++ JUNGOL/Main_J_Ad_1863_종교_정세린.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_J_Ad_1863_종교_정세린 {
+	
+	static int[] parents;
+	
+	private static void makeSet(int n) {
+		parents = new int[n+1];
+		Arrays.fill(parents, -1);
+	}
+	
+	private static int findSet(int a) {
+		if (parents[a] < 0) return a;
+		return parents[a] = findSet(parents[a]);
+	}
+	
+	private static boolean unionSet(int a, int b) {
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		if (aRoot == bRoot) return false;
+		parents[bRoot] = aRoot;
+		return true;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N, M, cnt = 0;
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		makeSet(N);
+		
+		for (int i = 1; i <= M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			unionSet(a, b);
+		}
+		 Arrays.sort(parents);
+		 
+		 for (int i = 1; i <= N; i++) {
+			 if (parents[i] == -1) cnt++;
+		 }
+		 System.out.println(cnt);
+		
+	}
+}
