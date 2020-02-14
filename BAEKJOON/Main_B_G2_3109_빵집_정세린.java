5e2bbc8bb2a6dcd36ad62d1cba40950265e7ab0a
Serin <tpfls410@gmail.com>
2020-02-14 07:10:51 +0000

    Upload New File

---
+++ BAEKJOON/Main_B_G2_3109_빵집_정세린.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_B_G2_3109_빵집_정세린 {
+	
+	static int[][] dh = {{-1, 1}, {0, 1}, {1, 1}};
+	static boolean[][] visited;
+	static char[][] map;
+	static int R, C, cnt = 0;
+	static boolean flag;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		String str;
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		visited = new boolean[R][C];
+		map = new char[R][C];
+		
+		
+		for(int i = 0; i < R; i++) {
+			str = (br.readLine());
+			map[i] = str.toCharArray();
+		}
+		
+		for (int i = 0; i < R; i++) {
+			flag = false;
+			if(!visited[i][0]) {
+				dfs(i, 0);
+			}
+		}
+		System.out.println(cnt);
+		
+	}
+	
+	private static void dfs(int i, int j) {
+		if (flag) return;
+		if (j == C-1) {
+			cnt++;
+			flag =true;
+		}
+		visited[i][j] = true;
+		for (int dir = 0; dir < 3; dir++) {
+			int itmp = i + dh[dir][0];
+			int jtmp = j + dh[dir][1];
+			if (itmp >= 0 && itmp < R && jtmp >= 0 && jtmp < C && !visited[itmp][jtmp] && map[itmp][jtmp] == '.')
+				dfs(itmp, jtmp);
+		}
+	}
+	
+}
