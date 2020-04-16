2f26a26c7933306fb9d90f01e706c628ea956533
Serin <tpfls410@gmail.com>
2020-04-16 09:43:12 +0000

    Upload New File

---
+++ BAEKJOON/Main_B_S3_2579_계단오르기_정세린.java
@@ -0,0 +1,47 @@
+/*
+ * 1279KB
+ * 76ms 
+ */
+
+package Baekjoon;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_B_S3_2579_계단오르기_정세린 {
+	static int[] stair;
+	static int N, max;
+	static int flag;
+	static int[][] memo;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		stair = new int[N + 1];
+		memo = new int[N + 1][2];
+		for (int i = 1; i < N + 1; i++) {
+			stair[i] = Integer.parseInt(br.readLine());
+		}
+		max = step(N, 1);
+		System.out.println(max);
+	}
+
+	private static int step(int index, int flag) {
+		if (index == 0 || index == 1) {
+			return memo[index][flag] = stair[index];
+		}
+		if (memo[index][flag] != 0)
+			return memo[index][flag];
+		if (flag == 0) {
+			return memo[index][flag] = step(index - 2, 1) + stair[index];
+		}
+		return memo[index][flag] = maximum(step(index - 1, 0) + stair[index], step(index - 2, 1) + stair[index]);
+	}
+
+	private static int maximum(int a, int b) {
+		return (a > b) ? a : b;
+	}
+
+}
