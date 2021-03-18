6bb64e9615d1eca155d15c3904beb993ab057821
Serin <tpfls410@gmail.com>
2021-03-19 03:00:29 +0900

    Solution_P_L2_64065_튜플_정세린.java: LinkedHashSet

---
+++ BAEKJOON/Main_B_B1_2839_설탕배달_정세린.java
@@ -0,0 +1,37 @@
+/*
+ * 11552KB
+ * 80ms
+ * 10m
+ */
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B_B1_2839_설탕배달_정세린 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if (N == 4) {
+			System.out.println(-1);
+			return;
+		}
+		int cnt = 0;
+
+		int n = N;
+		while (n % 5 != 0) {
+			n = n - 3;
+			cnt++;
+			if (n < 0) {
+				System.out.println(-1);
+				return;
+			}
+		}
+		cnt += n / 5;
+		
+		System.out.println(cnt);
+	} // end of main
+	
+}
