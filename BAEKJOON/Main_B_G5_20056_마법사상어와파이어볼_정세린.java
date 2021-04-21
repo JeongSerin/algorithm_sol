34b1a7be22686e15f239bc10414e2843093ff6d6
Serin <tpfls410@gmail.com>
2021-04-21 22:19:07 +0900

    Solution_P_L2_76502_괄호회전하기_정세린.java: stack

---
+++ BAEKJOON/Main_B_G5_20056_마법사상어와파이어볼_정세린.java
@@ -0,0 +1,13 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_B_G5_20056_마법사상어와파이어볼_정세린 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
