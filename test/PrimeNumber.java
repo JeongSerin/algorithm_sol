4c747b203ac14829d315daae869fb9a40e3d11e1
Serin <tpfls410@gmail.com>
2021-05-29 11:41:29 +0900

    Solution_P_L2_42577_전화번호목록_정세린.java: hashmap

--- test/PrimeNumber.java
+++ test/PrimeNumber.java
@@ -1,11 +1,16 @@
 package test;
 
-public class PrimeNumber {
+import java.util.Arrays;
 
+public class PrimeNumber {
+	static int MAX = 100000;
+	static boolean[] isPrime = new boolean[MAX + 1];
 	
 	public static void main(String[] args) {
 		boolean isPrimeNumber = isPrimeNumber(1);
 		System.out.println(isPrimeNumber);
+		
+		primeNumberSieve();
 	}
 	
 	static boolean isPrimeNumber(int n) {
@@ -14,6 +19,21 @@ public class PrimeNumber {
 			if (n % i == 0) return false;
 		}
 		return true;
+	}
+	
+	static void primeNumberSieve() {
+		Arrays.fill(isPrime, true);
+		
+		for (int i = 2; i <= MAX; i++) {
+			if (!isPrime[i]) continue;
+			for (int j = i + i; j <= MAX; j += i) {
+				isPrime[j] = false;
+			}
+		}
+		
+		for (int i = 2; i <= MAX; i++) {
+			if (isPrime[i]) System.out.println(i);
+		}
 	}
 
 }
